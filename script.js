6f0accd79cf0701b8a01fd240502c8158cffbe70
EajayD <edelos1524@hotmail.com>
2021-07-01 20:25:52 -0700

    added local storage set, deleted personal styling on html because didnt realized the pre built css for changing colors

--- script.js
+++ script.js
@@ -2,3 +2,18 @@
 var date = moment().format('dddd, MMMM Do, YYYY')
 $('#currentDay').text('Today is ' + date); //displays current date 
 
+// run function on start up
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        console.log(this);
+        var schedule = $(this).siblings(".schedule").val(); 
+        var hour = $(this).parent().attr("id"); 
+
+        console.log(schedule);
+        console.log(hour);
+
+        // local storage set
+        localStorage.setItem(hour, schedule);
+    })
+}) 
+
